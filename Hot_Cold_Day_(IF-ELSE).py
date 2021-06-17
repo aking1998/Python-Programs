81cb14a080e5a177b8b325de9c0dd1616caa617d
Aniket <73122782+aking1998@users.noreply.github.com>
2021-06-17 14:41:07 +0530

    Add files via upload

---
+++ Hot_Cold_Day_(IF-ELSE).py
@@ -0,0 +1,25 @@
+"""
+Problem Statement : Use if-elif-else concept
+ If it's hot
+    It's a hot day
+    Drink plenty of water
+ otherwise if its cold:
+    It's a cold day
+    Wear warm clothes
+ otherwise
+    It's a lovely day
+
+"""
+is_hot = False
+is_cold = True
+
+if is_hot:
+    print("It's a hot day")
+    print("Drink plenty of water")
+
+elif is_cold:
+    print("It's a cold day")
+    print("Wear warm clothes")
+
+else:
+    print("It's a lovely day")
