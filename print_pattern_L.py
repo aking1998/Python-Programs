674b50a2a9e1e20ec1be52693096f2a8f18ebea0
Aniket <73122782+aking1998@users.noreply.github.com>
2021-06-18 14:37:45 +0530

    Add files via upload

---
+++ print_pattern_L.py
@@ -0,0 +1,25 @@
+
+numbers = [2,2,2,2,7]
+
+for num_count in numbers:
+    output=''
+    for num in range(num_count):
+        output += 'x'
+    print(output)
+
+
+
+
+
+
+
+"""
+Print the following pattern:-
+
+    xx
+    xx   
+    xx   
+    xx   
+    xxxxxxx
+
+ """
